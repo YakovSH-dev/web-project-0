34d7b2fab47197f6404239702f762a98eeebedb9
Yakovsh <Yakov02@gmail.com>
2025-04-18 09:19:33 +0300

    small fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  // Add the base path for GitHub Pages deployment
+  // Replace '<your-repo-name>' with your actual GitHub repository name
+  base: '/web-project-0/', 
 })
